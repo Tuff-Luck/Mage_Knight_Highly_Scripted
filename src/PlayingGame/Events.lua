1aa3dc2ac9e1045d8fd6069880ba42066aaad41f
Tuff-Luck <72725951+Tuff-Luck@users.noreply.github.com>
2026-09-18 07:55:20 +0800

    Bump automatic error reporter to 413

--- src/PlayingGame/Events.lua
+++ src/PlayingGame/Events.lua
@@ -2996,7 +2996,7 @@ automaticLuaErrorSignatures={}
 automaticLuaErrorBreadcrumbs={}
 automaticLuaErrorBreadcrumbLimit=10
 automaticLuaErrorURL="https://script.google.com/macros/s/AKfycbzU1dSg2mafsUbUTNqOHce0cdWId2I8fkYiNO1JUgG73wtV9E2DCvm7uZ02bXviO-vnFw/exec"
-automaticLuaErrorReporterVersion="412"
+automaticLuaErrorReporterVersion="413"
 
 function automaticLuaErrorValue(callback, fallback)
 	local ok, value=pcall(callback)
