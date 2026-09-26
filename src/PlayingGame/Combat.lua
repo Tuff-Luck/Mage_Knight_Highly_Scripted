c093abe210d6fb779d5e4a0cbbf714b68db33b2f
Tuff-Luck <72725951+Tuff-Luck@users.noreply.github.com>
2026-09-26 12:43:24 +0800

    Retreat from the Fury Dragon current space

--- src/PlayingGame/Combat.lua
+++ src/PlayingGame/Combat.lua
@@ -1643,9 +1643,9 @@ function rewardRetreatRequired(playerIndex,avatarLocation,nearbyOwnShield)
 		end
 	end
 	local dragonRetreatRequired=false
-	if gStates.apocalypseDragonDefeated~=true and apocalypseDragonLairContainsPosition~=nil then
+	if gStates.apocalypseDragonDefeated~=true and apocalypseDragonCombatContainsPosition~=nil then
 		local dragonAvatarPos=mageKnightAvatarPosition(playerIndex)
-		dragonRetreatRequired=dragonAvatarPos~=nil and apocalypseDragonLairContainsPosition(dragonAvatarPos)==true
+		dragonRetreatRequired=dragonAvatarPos~=nil and apocalypseDragonCombatContainsPosition(dragonAvatarPos)==true
 	end
 	return dragonRetreatRequired==true or
 		((avatarLocation=="keep" or avatarLocation=="mage tower") and nearbyOwnShield=="false") or
